815c34acb7675e2a1908f73e215c166923382b4b
Donato Perfido <exdperfi@siag.it>
2025-04-03 14:44:41 +0000

    Aggiunta plugin JsonFormatters

--- devbuddy.common/Enums/ModulesItems.cs
+++ devbuddy.common/Enums/ModulesItems.cs
@@ -14,5 +14,8 @@ namespace devbuddy.common.Enums
 
         [ModuleKey("5ef20b73-22f7-4da1-a56a-e7a86853aa39")]
         Settings,
+
+        [ModuleKey("45e7bf11-4192-40c0-b4ea-b2701fa345f4")]
+        JsonFormatters,
     }
 }
---
+++ devbuddy.plugins/devbuddy.plugins.JsonUtilities/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using devbuddy.common.Attributes;
+using devbuddy.common.Enums;
+
+namespace devbuddy.plugins.JsonUtilities
+{
+
+    [DynamicallyLoadable(ModulesItems.JsonFormatters)]
+    public sealed partial class Index
+    {
+
+    }
+}

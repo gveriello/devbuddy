17456d78079c1ac14224f56584b007dcdbb65e9d
Donato Perfido <exdperfi@siag.it>
2025-04-03 14:22:17 +0000

    Aggiunta classe BuildInfo + rinomina dei progetti

---
+++ devbuddy.web/BuildInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace devbuddy.web
+{
+    public class BuildInfo
+    {
+        public const string BuildNumber = "DEV";
+        public const string BuildDate = "Local Environment";
+    }
+}
